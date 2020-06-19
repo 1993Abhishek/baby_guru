c666ae68a89c0f9605c1bc767b430191917aa4c2
Abhishek <abhishek.ghoshal.ncrts@gmail.com>
2020-06-19 13:21:04 +0530

    email_verification

--- lib/ui/screens/email_verification.dart
+++ lib/ui/screens/email_verification.dart
@@ -3,11 +3,11 @@ import 'package:babyguru/ui/helper/app_color.dart';
 import 'package:babyguru/ui/util/app_dimen.dart';
 import 'package:babyguru/ui/util/app_view_helper.dart';
 import 'package:babyguru/ui/widgets/custom_button.dart';
-import 'package:babyguru/ui/widgets/text_field_widget.dart';
+import 'package:babyguru/ui/widgets/otp_box_field.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class VerifyEmail extends StatefulWidget {
   @override
@@ -15,15 +15,34 @@ class VerifyEmail extends StatefulWidget {
 }
 
 class _VerifyEmailState extends State<VerifyEmail> {
-  FocusNode _nodeUserEmail;
 
-  TextEditingController _controllerEmail;
+  TextEditingController _controllerBlock1,
+      _controllerBlock2,
+      _controllerBlock3,
+      _controllerBlock4;
+
+  FocusNode _focusNodeBlock1,
+      _focusNodeBlock2,
+      _focusNodeBlock3,
+      _focusNodeBlock4;
+
+  String otpText1 = "",
+      otpText2 = "",
+      otpText3 = "",
+      otpText4 = "";
 
   @override
   void initState() {
-    _nodeUserEmail = FocusNode();
+    _controllerBlock1 = TextEditingController();
+    _controllerBlock2 = TextEditingController();
+    _controllerBlock3 = TextEditingController();
+    _controllerBlock4 = TextEditingController();
 
-    _controllerEmail = TextEditingController();
+    _focusNodeBlock1 = FocusNode();
+    _focusNodeBlock2 = FocusNode();
+    _focusNodeBlock3 = FocusNode();
+    _focusNodeBlock4 = FocusNode();
+    _focusNodeBlock4 = FocusNode();
 
     super.initState();
   }
@@ -48,105 +67,167 @@ class _VerifyEmailState extends State<VerifyEmail> {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.only(
-                top: vDimen(40),
-                bottom: vDimen(10),
-                left: hDimen(28),
-                right: hDimen(30),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      height: vDimen(40),
-                      width: vDimen(40),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white30,
-                          width: 1,
-                        ),
-                        color: AppColor.colorLogInScreenFields,
-                        borderRadius: BorderRadius.circular(10),
+          Padding(
+            padding: EdgeInsets.only(
+              top: vDimen(40),
+              bottom: vDimen(10),
+              left: hDimen(28),
+              right: hDimen(30),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    height: vDimen(40),
+                    width: vDimen(40),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white30,
+                        width: 1,
                       ),
-                      child: Center(
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white70,
-                        ),
+                      color: AppColor.colorLogInScreenFields,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white70,
                       ),
                     ),
                   ),
-                  vSpacing(35),
-                  Text(
-                    "Varify your email",
-                    style: TextStyle(fontSize: hDimen(40), color: Colors.white),
+                ),
+                vSpacing(35),
+                Text(
+                  "Varify your email",
+                  style: TextStyle(fontSize: hDimen(40), color: Colors.white),
+                ),
+                vSpacing(10),
+                Text(
+                  "Please enter the code sent to your",
+                  style: TextStyle(fontSize: hDimen(19), color: Colors.white),
+                ),
+                vSpacing(5),
+                Text(
+                  "email id",
+                  style: TextStyle(fontSize: hDimen(19), color: Colors.white),
+                ),
+                vSpacing(140),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: hDimen(20),
+                    right: hDimen(10),
                   ),
-                  vSpacing(10),
-                  Text(
-                    "Please enter the code sent to your",
-                    style: TextStyle(fontSize: hDimen(19), color: Colors.white),
-                  ),
-                  vSpacing(5),
-                  Text(
-                    "email id",
-                    style: TextStyle(fontSize: hDimen(19), color: Colors.white),
-                  ),
-                  vSpacing(140),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: hDimen(20),
-                      right: hDimen(10),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        CustomTextField(
-                          icon: Icons.check,
-                          labelText: "Email",
-                          image: " ",
-                          maxLength: 50,
-                          focusNode: _nodeUserEmail,
-                          backgroundColor: AppColor.colorLogInScreenFields,
-                          hintText: "Email",
-                          inputAction: TextInputAction.next,
-                          obSecure: false,
-                          textEditingController: _controllerEmail,
-                          textStyle: TextStyle(
-                            fontSize: vDimen(16),
-                            color: Colors.white70,
-                          ),
-                          onSubmit: (value) {
-                            _nodeUserEmail.unfocus();
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Enter Code",style: TextStyle(color: Colors.white70,fontSize: hDimen(16)),),
+                      vSpacing((vDimen(10))),
+                      Row(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
+                        OTPBoxField(
+                          height: vDimen(65),
+                          width: vDimen(65),
+                          textEditingController: _controllerBlock1,
+                          focusNode: _focusNodeBlock1,
+                          textInputAction: TextInputAction.next,
+                          textInputType: TextInputType.number,
+                          hasError:otpText1.isEmpty,
+                          onChange: (value) {
+                            otpText1 = value;
+                            if (value
+                                .toString()
+                                .length > 0) {
+                              _focusNodeBlock1.unfocus();
+                              _focusNodeBlock2.requestFocus();
+                            }
                           },
                         ),
-                        vSpacing((vDimen(20))),
-                        Text(
-                          "Resend Code",
-                          style: TextStyle(
-                            fontSize: hDimen(15),
-                            color: Colors.black,
-                            decoration: TextDecoration.underline,
-                          ),
+                        hSpacing(10),
+                        OTPBoxField(
+                          textEditingController: _controllerBlock2,
+                          focusNode: _focusNodeBlock2,
+                          hasError:otpText2.isEmpty,
+                          onChange: (value) {
+                            otpText2 = value;
+                            if (value
+                                .toString()
+                                .length > 0) {
+                              _focusNodeBlock2.unfocus();
+                              _focusNodeBlock3.requestFocus();
+                            } else {
+                              _focusNodeBlock2.unfocus();
+                              _focusNodeBlock1.requestFocus();
+                            }
+                          },
+                          textInputAction: TextInputAction.next,
+                          textInputType: TextInputType.number,
                         ),
-                        vSpacing(35),
-                        CustomButton(
-                          buttonText: "Reset Password",
-                          onClick: () {
-                            Navigator.of(context)
-                                .pushReplacementNamed(Router.ROUTE_HOME);
+                        hSpacing(10),
+                        OTPBoxField(
+                          textEditingController: _controllerBlock3,
+                          focusNode: _focusNodeBlock3,
+                          textInputAction: TextInputAction.next,
+                          textInputType: TextInputType.number,
+                          hasError:otpText3.isEmpty,
+                          onChange: (value) {
+                            otpText3 = value;
+                            if (value
+                                .toString()
+                                .length > 0) {
+                              _focusNodeBlock3.unfocus();
+                              _focusNodeBlock4.requestFocus();
+                            } else {
+                              _focusNodeBlock3.unfocus();
+                              _focusNodeBlock2.requestFocus();
+                            }
                           },
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                        hSpacing(10),
+                        OTPBoxField(
+                          textEditingController: _controllerBlock4,
+                          focusNode: _focusNodeBlock4,
+                          hasError: otpText4.isEmpty,
+                          textInputType: TextInputType.number,
+                          onChange: (value) {
+                            otpText4 = value;
+                            if (value
+                                .toString()
+                                .length > 0) {
+                              _focusNodeBlock4.unfocus();
+//                              _focusNodeBlock5.requestFocus();
+                            } else {
+                              _focusNodeBlock3.requestFocus();
+                            }
+                            /*Timer(Duration(milliseconds: 600), () =>
+                              Navigator.popUntil(context,
+                                  ModalRoute.withName(Router.ROUTE_LOGIN)));*/
+                          },
+                        ),
+                      ],),
+                      vSpacing(20),
+                      Text(
+                        "Resend Code",
+                        style: TextStyle(
+                          fontSize: hDimen(15),
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                      vSpacing(35),
+                      CustomButton(
+                        buttonText: "Reset Password",
+                        onClick: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed(Router.ROUTE_HOME);
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ],
---
+++ lib/ui/widgets/otp_box_field.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:babyguru/ui/util/app_dimen.dart';
+
+class OTPBoxField extends StatefulWidget {
+  final String hintText;
+  final String valueText;
+  final TextInputAction textInputAction;
+  final TextInputType textInputType;
+  final bool isObscureText;
+  final TextEditingController textEditingController;
+  final FocusNode focusNode;
+  final Function onSubmitted;
+  final Function onChange;
+  final Function onTap;
+  final bool isReadOnly;
+  final double height;
+  final double width;
+  final TextStyle textStyle;
+  final double borderRadius;
+  final bool hasError;
+
+  const OTPBoxField(
+      {Key key,
+        this.valueText,
+        this.hintText,
+        this.textStyle,
+        this.textInputAction = TextInputAction.done,
+        this.textInputType = TextInputType.text,
+        this.isObscureText = false,
+        this.textEditingController,
+        this.focusNode,
+        this.onChange,
+        this.onTap,
+        this.height,
+        this.width,
+        this.hasError = false,
+        this.isReadOnly = false,
+        this.borderRadius,
+        this.onSubmitted})
+      : super(key: key);
+
+  @override
+  _OTPBoxField createState() => _OTPBoxField();
+}
+
+class _OTPBoxField extends State<OTPBoxField> {
+  TextEditingController _textEditingController;
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    _textEditingController = widget.textEditingController == null
+        ? TextEditingController()
+        : widget.textEditingController;
+    if (widget.valueText != null)
+      _textEditingController.text = widget.valueText;
+    _focusNode = widget.focusNode == null ? FocusNode() : widget.focusNode;
+    _focusNode.addListener(_focusChangeListener);
+    super.initState();
+  }
+
+  void _focusChangeListener() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _focusNode.requestFocus(),
+      child: Container(
+        height: widget.height != null ? widget.height : hDimen(50),
+        width: widget.width != null ? widget.width : hDimen(50),
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: widget.hasError
+                    ? Colors.white
+                    : _focusNode.hasFocus
+                    ? Colors.white
+                    : Colors.white70,
+                width: _focusNode.hasFocus ? 2 : 1.8),
+            borderRadius: BorderRadius.circular(widget.borderRadius != null
+                ? widget.borderRadius
+                : hDimen(10)),
+            color: Colors.white),
+        child: Center(
+          child: TextField(
+              textAlign: TextAlign.center,
+              cursorColor: Colors.black,
+              focusNode: _focusNode,
+              controller: _textEditingController,
+              textInputAction: widget.textInputAction,
+              maxLength: 1,
+              maxLines: 1,
+              style: widget.textStyle != null
+                  ? widget.textStyle
+                  : TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: hDimen(22),
+              ),
+              onSubmitted: (value) {
+                if (widget.onSubmitted != null) widget.onSubmitted(value);
+              },
+              onChanged: (value) {
+                if (widget.onChange != null) widget.onChange(value);
+              },
+              onTap: () {
+                if (widget.onTap != null) widget.onTap();
+              },
+              keyboardType: widget.textInputType,
+              obscureText: widget.isObscureText,
+              readOnly: widget.isReadOnly,
+              decoration: InputDecoration(
+                counterText: "",
+                contentPadding: EdgeInsets.all(0),
+                border: InputBorder.none,
+                hintText: widget.hintText == null ? "" : widget.hintText,
+                hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: hDimen(18)),
+              )),
+        ),
+      ),
+    );
+  }
+}
